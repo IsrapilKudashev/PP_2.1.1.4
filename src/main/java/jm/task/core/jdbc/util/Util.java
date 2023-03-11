2eba0ff1dc08dbefd373bb1bedc1cfccade149d2
IsrapilKudashev <kudashev.020@gmail.com>
2023-03-11 15:41:42 +0300

    fix Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:musql://localhost:3306/user";
+    private static final String URL = "jdbc:mysql://localhost:3306/user";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
